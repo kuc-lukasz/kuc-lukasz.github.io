dc28764bead799e282da07a266de34ac64097ca1
Luka Ku <lukaszkuc93@gmail.com>
2021-11-30 19:43:01 +0100

    project add queryselectors

--- projects.js
+++ projects.js
@@ -1,34 +1,25 @@
-const blueCloseBtn = document.querySelector(".close-bottom-blue");
+const projectsWindow = document.querySelector('.main-container-projects')
+
+const blueCloseBtn = document.querySelector('.close-bottom-blue');
 const singleProjectWindow = document.querySelector(".single-project-window");
-const singleProjects = document.querySelector(".single-project");
+const allSingleProjectWindows = document.querySelectorAll(".single-project-window");
+const singleProject = document.querySelector(".single-project");
 const allSingleProjects = document.querySelectorAll(".single-project");
 
-// blueCloseBtn.addEventListener('click', () => {
-//     console.log('dziala')
-// projectWindow.classList.remove('display')
-// projectWindow.classList.add('hide-display')
-// })
-
-// singleProject.addEventListener('click', ()=>{
-//     projectWindow.classList.remove('hide-display')
-//     projectWindow.classList.add('display')
-// })
-
-allSingleProjects.forEach((singleProject) => {
- 
-  const blueCloseBtn = document.querySelector(".close-bottom-blue");
-
-  blueCloseBtn.addEventListener("click", closeSingleWindow);
-  singleProject.addEventListener("click", openSingleWindows);
-  
-});
-
-function closeSingleWindow() {
+allSingleProjects.forEach((singleProject)=>{
+const oknoZprojektem = singleProject.nextElementSibling
+function openWindow (){
     console.log('dziala')
-    singleProjectWindow.classList.remove("view")
-  singleProjectWindow.classList.add("hide-display");
+    oknoZprojektem.classList.add('view')
 }
-function openSingleWindows() {
-    singleProjectWindow.classList.remove("hide-display");
-    singleProjectWindow.classList.add("view");
+singleProject.addEventListener('click', openWindow)
+})
+
+allSingleProjectWindows.forEach((singleProjectWindow)=>{
+const closingBtn = singleProjectWindow.querySelector('.close-bottom-blue')
+function closingWindow(){
+    singleProjectWindow.classList.remove('view')
 }
+closingBtn.addEventListener('click', closingWindow)
+
+})
