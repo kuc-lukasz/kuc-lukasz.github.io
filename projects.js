f0b1de2e2a421600efa10f7a546a389f71d70263
Luka Ku <lukaszkuc93@gmail.com>
2021-11-30 10:36:14 +0100

    add querySelectorAll

--- projects.js
+++ projects.js
@@ -1,16 +1,34 @@
-const blueCloseBtn = document.querySelector(".close-bottom-blue")
-const projectWindow = document.querySelector('.single-project-window')
-const singleProject = document.querySelector('.single-project')
+const blueCloseBtn = document.querySelector(".close-bottom-blue");
+const singleProjectWindow = document.querySelector(".single-project-window");
+const singleProjects = document.querySelector(".single-project");
+const allSingleProjects = document.querySelectorAll(".single-project");
 
-blueCloseBtn.addEventListener('click', () => {
+// blueCloseBtn.addEventListener('click', () => {
+//     console.log('dziala')
+// projectWindow.classList.remove('display')
+// projectWindow.classList.add('hide-display')
+// })
+
+// singleProject.addEventListener('click', ()=>{
+//     projectWindow.classList.remove('hide-display')
+//     projectWindow.classList.add('display')
+// })
+
+allSingleProjects.forEach((singleProject) => {
+ 
+  const blueCloseBtn = document.querySelector(".close-bottom-blue");
+
+  blueCloseBtn.addEventListener("click", closeSingleWindow);
+  singleProject.addEventListener("click", openSingleWindows);
+  
+});
+
+function closeSingleWindow() {
     console.log('dziala')
-projectWindow.classList.remove('display')
-projectWindow.classList.add('hide-display')
-})
-
-singleProject.addEventListener('click', ()=>{
-
-    console.log('dziala')
-    projectWindow.classList.remove('hide-display')
-    projectWindow.classList.add('display')
-})
+    singleProjectWindow.classList.remove("view")
+  singleProjectWindow.classList.add("hide-display");
+}
+function openSingleWindows() {
+    singleProjectWindow.classList.remove("hide-display");
+    singleProjectWindow.classList.add("view");
+}
